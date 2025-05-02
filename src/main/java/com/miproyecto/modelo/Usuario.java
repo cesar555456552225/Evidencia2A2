5a617e4aba8eaded938273b18ee5f2ef87a67833
Personal <Personal@SOGAPRCMRFSD408.FormacionSena.RED>
2025-05-02 08:24:37 -0500

    se agrego el geter and seter en usuario.java

--- src/main/java/com/miproyecto/modelo/Usuario.java
+++ src/main/java/com/miproyecto/modelo/Usuario.java
@@ -10,4 +10,46 @@ package com.miproyecto.modelo;
  */
 public class Usuario {
     
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", apellido=" + apellido + ", email="+ email + '}';
+    }
 }
